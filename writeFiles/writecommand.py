3420baedb16f844a1e713b49c3020791b08c98b3
luffylg <lake0105@foxmail.com>
2017-05-14 21:22:02 +0800

    CRLF to LF

--- writeFiles/writecommand.py
+++ writeFiles/writecommand.py
@@ -3,20 +3,27 @@ import globalvar as gv
 
 def writecommand():
     with open(os.path.join(gv.basedir,'allrun'),'wb') as cmdfile:
-        cmdfile.write('#!/bin/sh\n')
-        cmdfile.write('# Source tutorial run functions\n')
-        cmdfile.write('. $WM_PROJECT_DIR/bin/tools/RunFunctions\n')
-        cmdfile.write('\n')
-        cmdfile.write('# Set application name\n')
-        cmdfile.write('application="interFoam"\n')
+        cmdfile.write('#!/bin/sh\n'.encode())
+        cmdfile.write('# Source tutorial run functions\n'.encode())
+        cmdfile.write('. $WM_PROJECT_DIR/bin/tools/RunFunctions\n'.encode())
+        cmdfile.write('\n'.encode())
+        cmdfile.write('# Set application name\n'.encode())
+        cmdfile.write('application="interFoam"\n'.encode())
         for lines in gv.files:
-            cmdfile.write('\n')
-            cmdfile.write('cd %s\n' % lines)
-            cmdfile.write('    runApplication blockMesh\n')
-            cmdfile.write('    runApplication setFields\n')
-            cmdfile.write('    runApplication decomposePar\n')
-            cmdfile.write('    runParallel $application 4\n')
-            cmdfile.write('    runApplication reconstructPar\n')
-            cmdfile.write('    runApplication sample\n')
-            # cmdfile.write('    runApplication foamToTecplot360\n')
-            cmdfile.write('cd ..\n')
+            cmdfile.write('\n'.encode())
+            cmdfile.write(('cd %s\n' % lines).encode())
+            cmdfile.write('    runApplication blockMesh\n'.encode())
+            cmdfile.write('    runApplication setFields\n'.encode())
+            cmdfile.write('    runApplication interFoam\n'.encode())
+            # cmdfile.write('    runApplication decomposePar\n'.encode())
+            # cmdfile.write('    runParallel $application 4\n'.encode())
+            # cmdfile.write('    runApplication reconstructPar\n'.encode())
+            cmdfile.write('    runApplication sample\n'.encode())
+            # cmdfile.write('    runApplication foamToTecplot360\n'.encode())
+            cmdfile.write('cd ..\n'.encode())
+    # with open(os.path.join(gv.basedir,'allrun'),'rb') as cmdfile:
+    #     bys=cmdfile.readlines()
+    # with open(os.path.join(gv.basedir,'allrun'),'wb') as wfile:
+    #     wfile.write(bys)
+
+
